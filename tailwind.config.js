b7d6805987b9ef3e49792b0f6a95728af44a95bd
Iair <i.cywiner@wolfsohn.edu.ar>
2025-10-21 15:43:20 -0300

    inicio de actividades

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Ajusta esto según tu proyecto
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
